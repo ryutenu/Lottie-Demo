d25f0492a0c3351da78183b7a72b6be6572f0dcf
Alan Liu <ryutenu.tokyo@gmail.com>
2021-12-08 13:36:41 +0900

    [Mod] Play animation

--- Lottie-Demo/ViewController.swift
+++ Lottie-Demo/ViewController.swift
@@ -13,5 +13,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        playAnimation()
+    }
+    
+    private func playAnimation() {
+        // Create an animation view
+        let animationView = AnimationView(name: "santa-pop-up")
+        animationView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
+        animationView.contentMode = .scaleAspectFit
+        
+        // Set the animation style
+        animationView.loopMode = .playOnce
+        animationView.animationSpeed = 1
+        
+        // Add animation view to super view
+        view.addSubview(animationView)
+        
+        // Play animation once then remove it from super view
+        animationView.play { finished in
+            if finished {
+                animationView.removeFromSuperview()
+            }
+        }
     }
 }
