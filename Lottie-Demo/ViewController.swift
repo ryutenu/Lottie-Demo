956c35908387030d6307089036734ea80715b390
Alan Liu <ryutenu.tokyo@gmail.com>
2021-12-08 12:54:05 +0900

    [Add] First commit

---
+++ Lottie-Demo/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Lottie-Demo
+//
+//  Created by Alan Liu on 2021/12/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
